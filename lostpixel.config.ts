428fdcf61e3040fafcc49188227c5f9cfafad13d
d-ivashchuk <dimaiv@me.com>
2022-07-29 13:13:04 +0200

    add ladle integration

---
+++ lostpixel.config.ts
@@ -0,0 +1,8 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  ladleShots: {
+    ladleUrl: "http://localhost:61000",
+  },
+  generateOnly: true,
+};
